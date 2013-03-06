0e71ad9242473bf4dd91d119072c6444e89664fd
BELMAHFOUD Nizar <belmahfoud.nizar@gmail.com>
2013-03-06 05:45:20 +0100

    4 th commit

--- js/main.js
+++ js/main.js
@@ -58,27 +58,26 @@ App.collections.NoteCollection = Backbone.Collection.extend({
 			var collection = this;
 		// When localStorage updates, fetch data from the store
 			//this.localStorage.bind('update', function(){
-			//collection.fetch();})
+			collection.fetch();
+			//})
 		}
 	});	
 	
 //--------------------------------Path Model------------------------------
 
 	App.models.PathModel = Backbone.Model.extend({
-	    model :App.models.NoteModel,
-		
+	
 		defaults:{
 		name: null,
-		description : null
+		description : null,
+		notes : null
 		},
 	
 		localStorage: App.stores.Geonotes,
 		
 		initialize: function(){
-		this.localStorage.bind('update', function(){
-			collection.fetch();})
-		console.log("l'initialisation de  PathModel");	
-			
+		//this.notes.fetch();
+		console.log("l'initialisation de  PathModel");		
 		}
 	});
 	
@@ -97,7 +96,8 @@ App.collections.NoteCollection = Backbone.Collection.extend({
 
 		// When localStorage updates, fetch data from the store
 			//this.localStorage.bind('update', function(){
-			//collection.fetch();})
+			collection.fetch();
+			//})
 		}
 	});
 
@@ -202,125 +202,63 @@ App.views.AdminNotePage = Backbone.View.extend({
 		console.log(this.model);
 		console.log("dans la fonction render de note page Admin 2");
         this.listView = new App.views.AdminNoteList({el: $('ul', this.el), model: this.model});
-		console.log(this.listView.el);
         this.listView.render();
         return this;
     },
 	
 	events:{
-        'click #edit': 'edit',
         'click #btn': 'addNote',
-		'click #save': 'updateNote',
     },
 	
-    edit:function(){ 
-        console.log("dans la fonction edit de admin note page");    
-        console.log("edit");             
-        console.log(this.model);  
-        $('#latitude').val(this.model.get('latitude'));
-        $('#longitude').val(this.model.get('longitude'));
-        $('#description').val(this.model.get('Description'));
-        $('#name').val(this.model.get('name'));
-		
-    },
     addNote : function(){
 	    console.log("dans la fonction addNote de admin note page");
-		navigator.geolocation.getCurrentPosition(function(position)
-            {            
-				$('#latitude').val(position.coords.latitude);
-				$('#longitude').val(position.coords.longitude);
+		self=this;
+		var Latitude, Longitude;
+		console.log("avant la localisation");
+		if (!navigator.geolocation) {
+			alert('Erreur : votre navigateur ne supporte pas Geolocation');
+			return;
+		}
+		else navigator.geolocation.getCurrentPosition(function(position)
+            { 	
+				console.log("dans la localisation");			
+				self.Longitude = position.coords.longitude;
+				self.Latitude = position.coords.latitude;
+				var name = $('#notename').val();
+			    var description = $('#Description').val();
+				var note= new App.models.NoteModel({name: name,Description:description,longitude:self.Longitude, latitude:self.Latitude });
+				note.save();
+				console.log("Lat:"+self.Latitude+"    Lon:"+self.Longitude);
+				self.model.add(note);
+				console.log(note.get("id"));
+				console.log("à la fin de la fonction add note");
 			});
-			var name = $('#notename').val();
-			var description = $('#Description').val();
-			console.log("ajout de note :");
-			var Longitude = $('#longitude').val();
-			var Latitude = $('#latitude').val();
-			console.log(Longitude);
-			console.log(Latitude);
-			var note= new App.models.NoteModel({name: name,Description:description,longitude:Longitude, latitude:Latitude });
-			console.log(note);
-			note.save();
-			this.model.add(note);
-			$('#name').val(name);
-			$('#description').val(description); 
-
-		
-    },
-    updateNote: function(){
-        console.log("mise à jour de la note:  ");
-        var description=$('#description').val();
-        var notename=$('#name').val();
-        this.model.set({Description:description, name:notename});
-		console.log(this.model);
     }
 });
 //---------------------------------------------------------------------------------------------
 
-//----------------------------------------- Note view -----------------------------------------
-//---------------------------------------------------------------------------------------------
-
-//-----------------------------------------Add Note view --------------------------------------
-App.views.AddNote = Backbone.View.extend({
-	el: '#addNote',
-
-	events: {
-		'submit': 'submit'
-	},
-
-	submit: function(e) {
-		e.preventDefault();
-		var newNoteTitle = $(e.currentTarget).find('input[type=text]').val();
-		// Homework #2
-		// Below, we've separated this process into two steps.
-		// Remember, though, that a collection can also accept a simple
-		// object, because we've already defined what type of Model
-		// to associated it with. Combine the two lines below into one.
-		var note = new App.models.NoteModel({ name: newNoteTitle });
-		this.collection.add(note);
-		// Homework #3
-		// What if, instead of creating the model as we did above,
-		// you abstracted this functionality away to its own method?
-		// How would you do that?
-	}
-});
-
-//---------------------------------------------------------------------------------------------
 //----------------------------------- Admin Note List View-------------------------------------
 
 App.views.AdminNoteList = Backbone.View.extend({
 	tagName: "ul",
     initialize: function() {
-		_.bindAll(this, 'addOne', 'addAll');
         this.model.bind("reset", this.render, this);
         this.model.on('add',this.addOne,this);
-		this.model.on('change',this.addOne,this);
-		this.model.bind('refresh', this.addAll);
     },
     render: function(eventName) {
         $(this.el).empty();
         console.log(this.model);
         _.each(this.model.models, function(note) {
-            $(this.el).append(new App.views.AdminNoteItem({model: note}).render().el);
-			$('#name').val(note.get('name'));
-			$('#description').val(note.get('Description')); 
-			$('#latitude').val(note.get('latitude'));
-			$('#longitude').val(note.get('longitude')); 
+            $(this.el).prepend(new App.views.AdminNoteItem({model: note}).render().el);
         }, this);
-        var map_view = new App.views.Map({
-         el: $('#map')[0],     
-          collection:this.model,
-        });
+		console.log("render admin  note list");
         return this;
     },
     addOne: function(note){
         console.log('ajout view note ');
         var noteView = new App.views.AdminNoteItem({ model: note });
         this.$el.prepend(noteView.render().el);
-    },
-	addAll: function(){
-		$(this.el).empty();
-		this.model.each(this.addOne);
-	}
+    }
 });
 //---------------------------------------------------------------------------------------------
 
@@ -328,18 +266,34 @@ App.views.AdminNoteList = Backbone.View.extend({
 App.views.AdminNoteItem = Backbone.View.extend({
     tagName: "li",
     events: {
-        'click #delete': 'destroy'
+        'click #delete': 'destroy',
+		'click #save' : 'save',
     },
     initialize: function() {
-		_.bindAll(this, 'render')
         this.template = _.template(App.utils.templateLoader.get('admin-note-item'));
         this.model.on('change', this.render, this);
         this.model.on('destroy', this.remove, this);
     },
     render: function(eventName) {
-        $(this.el).html(this.template(this.model.toJSON()));
-		return this;  
+	
+		$(this.el).html(this.template(this.model.toJSON()));
+		return this; 
+
     },
+	
+	save: function(){
+
+	     NewName = document.getElementById("name "+this.model.get('id')).value;
+		 NewDescription = document.getElementById("description "+this.model.get('id')).value;
+         /*this.model.save({
+             name: NewName,
+			 Description : NewDescription
+         });*/
+		 this.model.set('name',NewName);
+		 this.model.set('Description',NewDescription);
+		 this.model.save();
+		 alert("Note updates were successful");
+	},
     destroy: function() {
         this.model.destroy();
     },
@@ -348,10 +302,106 @@ App.views.AdminNoteItem = Backbone.View.extend({
     }
 
 });
-//-------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 
-//--------------------------------------Map View------------------------------------------
+//----------------------------------------- Admin Path Page -----------------------------------
+App.views.AdminPathPage = Backbone.View.extend({
+    templateLoader: App.utils.templateLoader,
+    initialize: function() {
+		console.log("l'initialisation de  AdminNotePage");
+		this.template = _.template(this.templateLoader.get('admin-path-page'));
+    },
 
+    render: function(eventName) {
+	    console.log("dans la fonction render de admin path page");
+        $(this.el).html(this.template(this.model.toJSON()));
+        this.listView = new App.views.AdminPathList({el: $('ul', this.el), model: this.model});
+        this.listView.render();
+        return this;
+    },
+	
+	events:{
+        'click #btn': 'addPath',
+    },
+	
+    addPath : function(){
+	    console.log("dans la fonction addPath de admin note page");
+		self=this;
+		var name = $('#pathname').val();
+	    var description = $('#Description').val();
+		var path= new App.models.PathModel({name: name,Description:description});
+		path.save();
+		self.model.add(path);
+		console.log("à la fin de la fonction add note");
+		}
+	});
+
+//---------------------------------------------------------------------------------------------
+
+//----------------------------------------- Admin Path List View ------------------------------
+App.views.AdminPathList = Backbone.View.extend({
+	tagName: "ul",
+    initialize: function() {
+        this.model.bind("reset", this.render, this);
+        this.model.on('add',this.addOne,this);
+    },
+    render: function(eventName) {
+        $(this.el).empty();
+        console.log(this.model);
+        _.each(this.model.models, function(path) {
+            $(this.el).prepend(new App.views.AdminPathItem({model: path}).render().el);
+        }, this);
+		console.log("render admin  note list");
+        return this;
+    },
+    addOne: function(path){
+        console.log('ajout view path ');
+        var pathView = new App.views.AdminPathItem({ model: path });
+        this.$el.prepend(pathView.render().el);
+    }
+});
+//---------------------------------------------------------------------------------------------
+
+//----------------------------------------- Admin Path Item -----------------------------------
+App.views.AdminPathItem = Backbone.View.extend({
+    tagName: "li",
+    events: {
+        'click #delete': 'destroy',
+		'click #save' : 'save',
+    },
+    initialize: function() {
+        this.template = _.template(App.utils.templateLoader.get('admin-path-item'));
+        this.model.on('change', this.render, this);
+        this.model.on('destroy', this.remove, this);
+    },
+    render: function(eventName) {
+	
+		$(this.el).html(this.template(this.model.toJSON()));
+		return this; 
+
+    },
+	
+	save: function(){
+
+	     NewName = document.getElementById("name "+this.model.get('id')).value;
+		 NewDescription = document.getElementById("description "+this.model.get('id')).value;
+		 this.model.set('name',NewName);
+		 this.model.set('Description',NewDescription);
+		 this.model.save();
+		 alert("Path updates : successful");
+	},
+    destroy: function() {
+        this.model.destroy();
+    },
+    remove: function() {
+        this.$el.remove();
+    }
+
+});
+//---------------------------------------------------------------------------------------------
+
+//--------------------------------------Map View-----------------------------------------------
+/*
 App.views.Map  = Backbone.View.extend({
     
         initialize: function() {
@@ -412,7 +462,7 @@ App.views.Map  = Backbone.View.extend({
 
 
 
-    });
+    });*/
 
 //---------------------------------------------------------------------------------------------
 
@@ -428,7 +478,8 @@ App.Router = Backbone.Router.extend({
 		"Home": "Home",
 		"admin-page": "adminpage",
 		"user-page": "userpage",
-		"noteAdmin": "noteAdmin"
+		"noteAdmin": "noteAdmin",
+		"pathAdmin": "pathAdmin"
 	},
 	
 	initialize: function() {
@@ -524,11 +575,22 @@ App.Router = Backbone.Router.extend({
 		this.noteadm.render();
         self.slidePage(this.noteadm);
     },
+	pathAdmin: function(){
+        console.log("dans la fonction router de path page admin");     
+        self = this;
+		var collection = new App.collections.PathCollection(App.stores.Geonotes.findAll());
+		console.log("après new pathCollection");
+		console.log(collection);
+		this.pathadm= new App.views.AdminPathPage({model: collection});
+		console.log(this.pathadm);
+		this.pathadm.render();
+        self.slidePage(this.pathadm);
+    }
 	
 });
 //-------------------------------------------------------------------------------------------
 
-App.utils.templateLoader.load(['home','admin-page','user-page','admin-note-page','admin-note-item'],
+App.utils.templateLoader.load(['home','admin-page','user-page','admin-note-page','admin-note-item','admin-path-page','admin-path-item'],
         function() {
             App.app = new App.Router();
             Backbone.history.start();
